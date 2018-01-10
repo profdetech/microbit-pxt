4a2a68191febd58899db36172ba7ef95254f22f4
profdetech <emmanuel.mouronval@gmail.com>
2018-01-10 17:09:48 +0100

    update

--- robot.ts
+++ robot.ts
@@ -47,4 +47,36 @@ namespace profdetech {
         pins.servoWritePin(AnalogPin.P0, 90);
         pins.servoWritePin(AnalogPin.P14, 180);
     }
+	
+	
+	/**
+     * ouvrir le portail (moteur sur P0)
+     */
+    //% subcategory=portail
+    //% blockId=portail_ouvrir
+    //% block="ouvrir le portail"
+    export function ouvrir(): void {
+        pins.servoWritePin(AnalogPin.P0, 0);
+    }
+
+    /**
+     * fermer le portail (moteur sur P0)
+     */
+    //% subcategory=portail
+    //% blockId=portail fermer
+    //% block="fermer le portail"
+    export function fermer(): void {
+        pins.servoWritePin(AnalogPin.P0, 180);
+    }
+	
+	/**
+     * arrêter le portail (moteur sur P0)
+     */
+    //% subcategory=portail
+    //% blockId=portail_arreter
+    //% block="arreter le portail"
+    export function PD(): void {
+        pins.servoWritePin(AnalogPin.P0, 90);
+    }
+
 }
