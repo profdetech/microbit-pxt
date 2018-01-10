46dd494098ce9d2aab4db44528a2c968a10fafb0
profdetech <emmanuel.mouronval@gmail.com>
2018-01-10 16:07:13 +0100

    basic fonction

--- robot.ts
+++ robot.ts
@@ -4,8 +4,8 @@ namespace profdetech {
 	* micro:bit mouronval
 	************************************************************************************************************************************************/
 
-        /**
-     * avancer
+    /**
+     * Faire avancer le robot
      */
     //% subcategory=robot
     //% blockId=robot_avancer
@@ -15,4 +15,36 @@ namespace profdetech {
         pins.servoWritePin(AnalogPin.P14, 180);
     }
 
-    
+    /**
+     * Faire reculer le robot
+     */
+    //% subcategory=robot
+    //% blockId=robot_reculer
+    //% block="reculer"
+    export function avancer(): void {
+        pins.servoWritePin(AnalogPin.P0, 180);
+        pins.servoWritePin(AnalogPin.P14, 0);
+    }
+	
+	/**
+     * Faire tourner à droite le robot
+     */
+    //% subcategory=robot
+    //% blockId=robot_TD
+    //% block="Tourner_a_droite"
+    export function TD(): void {
+        pins.servoWritePin(AnalogPin.P0, 0);
+        pins.servoWritePin(AnalogPin.P14, 90);
+    }
+
+    /**
+     * Faire tourner à gauche le robot
+     */
+    //% subcategory=robot
+    //% blockId=robot_TG
+    //% block="Touner_a_gauche"
+    export function TG(): void {
+        pins.servoWritePin(AnalogPin.P0, 90);
+        pins.servoWritePin(AnalogPin.P14, 180);
+    }
+}
