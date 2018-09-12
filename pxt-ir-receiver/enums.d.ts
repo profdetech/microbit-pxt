4c9729fe7c2d8bb22a53d4fa64a9bf59daeea2d9
profdetech <emmanuel.mouronval@gmail.com>
2018-09-12 07:53:19 +0200

    upd2

---
+++ pxt-ir-receiver/enums.d.ts
@@ -0,0 +1,53 @@
+// Auto-generated. Do not edit.
+
+
+    declare enum Pins {
+    P0 = 3,
+    P1 = 2,
+    P2 = 1,
+    P3 = 4,
+    P4 = 5,
+    P5 = 17,
+    P6 = 12,
+    P7 = 11,
+    P8 = 18,
+    P9 = 10,
+    P10 = 6,
+    P11 = 26,
+    P12 = 20,
+    P13 = 23,
+    P14 = 22,
+    P15 = 21,
+    P16 = 16,
+    P19 = 0,
+    P20 = 30,
+    }
+
+
+    declare enum RemoteButton {
+    A= 0x45,
+	B= 0x46,
+	C= 0x47,
+	D= 0x44,
+	E= 0x43,
+	F= 0xD,
+    Haut = 0x40,
+    Gauche = 0x7,
+    Bas = 0x19,
+    Droite = 0x9,
+	Parametre = 0x15,
+	Touche_0 = 0x16,
+	Touche_1 = 0xC,
+	Touche_2 = 0x18,
+	Touche_3 = 0x5E,
+	Touche_4 = 0x8,
+	Touche_5 = 0x1C,
+	Touche_6 = 0x5A,
+	Touche_7 = 0x42,
+	Touche_8 = 0x52,
+	Touche_9 = 0x4A,
+    }
+declare namespace Profdetech_IR {
+}
+
+// Auto-generated. Do not edit. Really.
